610f49a577809cc73d128bbc935e46deb7a9b52b
Erick <19063989@student.hhs.nl>
2024-03-14 14:01:54 +0100

    Fixed exit script

---
+++ Assets/Player/Scripts/ExitScript.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitScript : MonoBehaviour
+{
+    public GameObject exitObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        exitObject = GameObject.FindWithTag("Exit");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Inside the collision");
+        if (collision.gameObject.CompareTag("Player")){
+            #if UNITY_EDITOR
+                // Application.Quit() does not work in the editor so
+                // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+                UnityEditor.EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
+    }
+}
