81f43b4e78a087f8d03adb851e87113a990d1cf7
HunterFromYharnam <94044683+TudorGrigoras@users.noreply.github.com>
2024-03-15 16:27:16 +0100

    Added list to keep track of enemies hit

--- Assets/Player/Scripts/Weapon.cs
+++ Assets/Player/Scripts/Weapon.cs
@@ -7,7 +7,7 @@ public class Weapon : MonoBehaviour
     private CapsuleCollider swordCollider;
     public AudioSource swordSlashSound;
     public bool colliderSwitchOn_Off = false;
-    private List<GameObject> enemies = new List<GameObject>();
+    private List<GameObject> enemiesHit = new List<GameObject>();
     private bool strongAttack = false;
 
     public bool StrongAttack { get => strongAttack; set => strongAttack = value; }
@@ -23,7 +23,7 @@ public class Weapon : MonoBehaviour
     public void SwordToDefault(){
         GetComponent<MeshRenderer>().material.color = new Color32(255, 240, 0, 200); // Yellow Default
         swordCollider.enabled = false; // Disable Collider
-        enemies.Clear();
+        enemiesHit.Clear();
     }
 
     public void DisableSwordCollider() => swordCollider.enabled = false;
@@ -36,14 +36,14 @@ public class Weapon : MonoBehaviour
 
     public void OnTriggerEnter(Collider other)
     {
-        if (swordCollider.enabled && other.gameObject.CompareTag("Enemy") && !enemies.Any(x => x == other.gameObject))
+        if (swordCollider.enabled && other.gameObject.CompareTag("Enemy") && !enemiesHit.Any(x => x == other.gameObject))
         {
             other.gameObject.GetComponent<IDamageble>().Hit();
             if(strongAttack)
             {
                 other.gameObject.GetComponent<IDamageble>().ApplyKnockback();
             }
-            enemies.Add(other.gameObject);
+            enemiesHit.Add(other.gameObject);
         }
     }
 }
