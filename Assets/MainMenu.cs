bc86fa8142a9a0972a65c1f45ced5d3aa56d98e7
Erick <19063989@student.hhs.nl>
2024-03-19 20:21:16 +0100

    Main Menu Play Button Working

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+/* --- notes ---
+This sciript is called in the Play Button under "On Click ()"
+How to use this, attach the script on a GameObject so I created one called: MainMenuScriptObject
+Then drag and drop the MainMenuScriptObject on the "On Click ()" then select MainMenu then PlayGame
+*/
+public class MainMenu : MonoBehaviour
+{
+    void Start(){
+
+    }
+
+    void Update(){
+
+    }
+
+    public void PlayGame(){
+        SceneManager.LoadSceneAsync(1);
+    }
+}
