33952f9188595988134b443b17d0479c3e1962cd
Erick <19063989@student.hhs.nl>
2024-03-27 20:47:56 +0100

    Added Main Menu options, sound slider and screen resolution

---
+++ Assets/User Interface/MainMenu_Manager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu_Manager : MonoBehaviour
+{
+    public Canvas canvas;
+
+    void Start(){
+   
+    }
+
+    public void PlayGame(){
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void QuitGame(){
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
