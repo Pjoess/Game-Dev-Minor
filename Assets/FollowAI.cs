44582aa9144863d2eac96e6a8e362c8ea767bebd
Erick <19063989@student.hhs.nl>
2024-04-04 16:28:52 +0200

    File Cleanup

--- Assets/FollowAI.cs
+++ Assets/FollowAI.cs
@@ -1,31 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.AI;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.AI;
 
-public class followAI : MonoBehaviour
-{
-    public NavMeshAgent ai;
-    public Transform player;
-    public Animator aiAnim;
-    Vector3 dest;
+// public class followAI : MonoBehaviour
+// {
+//     public NavMeshAgent ai;
+//     public Transform player;
+//     public Animator aiAnim;
+//     Vector3 dest;
 
-    void Update()
-    {
-        dest = player.position;
-        ai.destination = dest;
-        if (ai.remainingDistance <= ai.stoppingDistance)
-        {
+//     void Update()
+//     {
+//         dest = player.position;
+//         ai.destination = dest;
+//         if (ai.remainingDistance <= ai.stoppingDistance)
+//         {
 
-            aiAnim.ResetTrigger("Run");
-            aiAnim.SetTrigger("Idle");
+//             aiAnim.ResetTrigger("Run");
+//             aiAnim.SetTrigger("Idle");
 
-        }
-        else
-        {
+//         }
+//         else
+//         {
 
-            aiAnim.ResetTrigger("Idle");
-            aiAnim.SetTrigger("Run");
-        }
-    }
-}
+//             aiAnim.ResetTrigger("Idle");
+//             aiAnim.SetTrigger("Run");
+//         }
+//     }
+// }
