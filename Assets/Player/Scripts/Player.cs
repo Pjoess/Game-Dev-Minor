0c6e9c404f23ab58ba0cf1c1bca520bc8bbd924e
HunterFromYharnam <94044683+TudorGrigoras@users.noreply.github.com>
2024-04-11 14:35:00 +0200

    Key Rebinding WIP

--- Assets/Player/Scripts/Player.cs
+++ Assets/Player/Scripts/Player.cs
@@ -314,7 +314,7 @@ public class Player : MonoBehaviour, IDamageble
             }
         }
 
-        void OnDash(InputValue value)
+        void OnDodge(InputValue value)
         {
             if (value.isPressed && !isDashing)  isDashing = true;
         }
