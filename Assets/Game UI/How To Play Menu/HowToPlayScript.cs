73c6bd2b368cac292c305b993cfa54a658e55e82
HunterFromYharnam <94044683+TudorGrigoras@users.noreply.github.com>
2024-04-12 20:26:16 +0200

    Menu imporvements
    -How to play will display current keybindings
    -player can't be controlled while in a menu

--- Assets/Game UI/How To Play Menu/HowToPlayScript.cs
+++ Assets/Game UI/How To Play Menu/HowToPlayScript.cs
@@ -1,17 +1,43 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HowToPlayScript : MonoBehaviour
 {
     [SerializeField] private GameObject closePanel;
+    private PlayerInput input;
+
+    [SerializeField] TMP_Text forwardsText;
+    [SerializeField] TMP_Text backwardsText;
+    [SerializeField] TMP_Text leftText;
+    [SerializeField] TMP_Text rightText;
+
+    [SerializeField] TMP_Text attackText;
+    [SerializeField] TMP_Text dodgeText;
+    [SerializeField] TMP_Text sprintText;
 
     void Start(){
         closePanel.SetActive(true);
+        input = FindObjectOfType<Player>().GetComponent<PlayerInput>();
+
+        forwardsText.text = input.actions.FindAction("Move").GetBindingDisplayString(2);
+        backwardsText.text = input.actions.FindAction("Move").GetBindingDisplayString(3);
+        leftText.text = input.actions.FindAction("Move").GetBindingDisplayString(4);
+        rightText.text = input.actions.FindAction("Move").GetBindingDisplayString(5);
+
+        attackText.text = input.actions.FindAction("Attack").GetBindingDisplayString();
+        dodgeText.text = input.actions.FindAction("Dodge").GetBindingDisplayString();
+        sprintText.text = input.actions.FindAction("Sprint").GetBindingDisplayString();
+        input.SwitchCurrentActionMap("UI");
+        Debug.Log(input.currentActionMap);
     }
 
     public void ClosePanel()
     {
         if(closePanel != null){
             closePanel.SetActive(false);
+            input.SwitchCurrentActionMap("Player");
+            Debug.Log(input.currentActionMap);
             Time.timeScale = 1;
         } else {
             closePanel.SetActive(true);
--- Assets/Player/Scripts/PauseMenu.cs
+++ Assets/Player/Scripts/PauseMenu.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject pauseMenu;
+    private PlayerInput input;
+    private void Start()
+    {
+        input = FindObjectOfType<Player>().GetComponent<PlayerInput>();
+    }
 
     public void EnablePauseCanvas(){
         if(Player.isPaused){
             pauseMenu.SetActive(true);
+            input.SwitchCurrentActionMap("UI");
+            Debug.Log(input.currentActionMap);
         } else {
             pauseMenu.SetActive(false);
+            input.SwitchCurrentActionMap("Player");
+            Debug.Log(input.currentActionMap);
         }
     }
 }
