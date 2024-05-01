249c95c7a2f4152283ebdb6bd6a4ef7425979063
Erick <19063989@student.hhs.nl>
2024-05-01 15:10:27 +0200

    Update Buddy_Blackboard.cs

--- Assets/The Game/Characters/Behaviour Tree/Buddy_Blackboard.cs
+++ Assets/The Game/Characters/Behaviour Tree/Buddy_Blackboard.cs
@@ -6,26 +6,11 @@ namespace buddy
     public class Blackboard : MonoBehaviour
     {
         public static Blackboard instance;
-        
         private Player_Manager player;
-
-        // public Animator animator;
-        // [HideInInspector] public int animIDWalk;
-        // [HideInInspector] public int animIDShooting;
-        // [HideInInspector] public int animIDShootingMortar;
-
-        public void AssignAnimIDs()
-        {
-            // animIDWalk = Animator.StringToHash("isWalking");
-            // animIDShooting = Animator.StringToHash("isShooting");
-            // animIDShootingMortar = Animator.StringToHash("isShootingMortar");
-        }
 
         private void Awake()
         {
             player = FindAnyObjectByType<Player_Manager>();
-            //animator = GameObject.Find("Buddy").GetComponent<Animator>();
-            AssignAnimIDs();
             instance = this;
         }
 
