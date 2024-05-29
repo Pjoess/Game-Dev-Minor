54e292f1a7b3f56c45719c9c58bf56026c9841d9
Erick <19063989@student.hhs.nl>
2024-05-29 21:53:31 +0200

    Update on UI video

--- Assets/The Game/Game UI/Main Menu/UI_Manager.cs
+++ Assets/The Game/Game UI/Main Menu/UI_Manager.cs
@@ -8,7 +8,6 @@ public class UI_Manager : MonoBehaviour
     public GameObject choicePanel;
     public GameObject MainMenuPanel;
     public VideoPlayer introVideo;
-    private AudioListener audioListener;
     private bool videoEnded = false;
 
     void Awake()
@@ -16,27 +15,8 @@ public class UI_Manager : MonoBehaviour
         LoadVolume();
     }
 
-    void Start()
-    {
-        // Get the main audio listener in the scene
-        audioListener = FindObjectOfType<AudioListener>();
-
-        // Check if this is a level or tutorial video
-        if (SceneManager.GetActiveScene().buildIndex == 1) // Assuming tutorial scene index is 1
-        {
-            // Subscribe to the tutorial video's completion event
-            introVideo.loopPointReached += OnTutorialVideoEnded;
-        }
-        else if (SceneManager.GetActiveScene().buildIndex == 2) // Assuming level scene index is 2
-        {
-            // Subscribe to the level video's completion event
-            introVideo.loopPointReached += OnLevelVideoEnded;
-        }
-    }
-
     void OnEnable()
     {
-        // Reset video when the GameObject is enabled
         ResetVideo();
     }
 
@@ -58,7 +38,7 @@ public class UI_Manager : MonoBehaviour
         }
         else
         {
-            AudioListener.volume = AudioListener.volume = PlayerPrefs.GetFloat("musicVolume");
+            AudioListener.volume = PlayerPrefs.GetFloat("musicVolume");
         }
     }
 
@@ -75,8 +55,10 @@ public class UI_Manager : MonoBehaviour
     void ResetVideo()
     {
         videoEnded = false;
-        introVideo.Stop();
-        introVideo.time = 0;
+        if(introVideo != null){
+            introVideo.Stop();
+            introVideo.time = 0;
+        }
     }
 
     void SkipVideo()
@@ -90,67 +72,45 @@ public class UI_Manager : MonoBehaviour
 
     public void PlayLevel()
     {
-    choicePanel.SetActive(false);
-    if (audioListener != null)
-        audioListener.enabled = false;
+        choicePanel.SetActive(false);
 
-    // Check if introVideo is not null before trying to use it
-    if (introVideo != null)
-    {
-        // If video has ended, reset it back to false and start from the beginning
-        if (videoEnded)
+        if (introVideo != null)
         {
-            ResetVideo();
+            if (videoEnded)
+            {
+                ResetVideo();
+            }
+            introVideo.Play();
         }
-
-        // Play the video
-        introVideo.Play();
-    }
-    
-    // Set up the event handler for playing levels
-    introVideo.loopPointReached += OnLevelVideoEnded;
+        introVideo.loopPointReached += OnLevelVideoEnded;
     }
 
     public void PlayTutorial()
     {
         choicePanel.SetActive(false);
-        if (audioListener != null)
-            audioListener.enabled = false;
 
-        // Check if introVideo is not null before trying to use it
         if (introVideo != null)
         {
-        // If video has ended, reset it back to false and start from the beginning
-        if (videoEnded)
-        {
-            ResetVideo();
+            if (videoEnded)
+            {
+                ResetVideo();
+            }
+            introVideo.Play();
         }
-
-        // Play the video
-        introVideo.Play();
-    }
-    
-    // Set up the event handler for playing tutorials
-    introVideo.loopPointReached += OnTutorialVideoEnded;
+        introVideo.loopPointReached += OnTutorialVideoEnded;
     }
 
     void OnLevelVideoEnded(VideoPlayer vp)
     {
         introVideo.loopPointReached -= OnLevelVideoEnded;
-        if (audioListener != null)
-            audioListener.enabled = true;
 
-        // Load the level scene (assuming it's scene index 2)
         SceneManager.LoadSceneAsync(2);
     }
 
     void OnTutorialVideoEnded(VideoPlayer vp)
     {
         introVideo.loopPointReached -= OnTutorialVideoEnded;
-        if (audioListener != null)
-            audioListener.enabled = true;
 
-        // Load the tutorial scene (assuming it's scene index 1)
         SceneManager.LoadSceneAsync(1);
     }
 
@@ -159,11 +119,13 @@ public class UI_Manager : MonoBehaviour
         return videoEnded;
     }
 
-    public void RestartLevel(){
+    public void RestartLevel()
+    {
         SceneManager.LoadSceneAsync(2);
     }
 
-    public void ChoiceToMainMenu(){
+    public void ChoiceToMainMenu()
+    {
         choicePanel.SetActive(false);
         MainMenuPanel.SetActive(true);
     }
