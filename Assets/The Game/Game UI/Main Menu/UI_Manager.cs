873dc981b16b7aea38194eb4d86267f4f1e2332d
Erick <19063989@student.hhs.nl>
2024-05-29 19:14:15 +0200

    Updated the play tutorial yes no then play video (still bug reset video when go back to menu)

--- Assets/The Game/Game UI/Main Menu/UI_Manager.cs
+++ Assets/The Game/Game UI/Main Menu/UI_Manager.cs
@@ -18,11 +18,20 @@ public class UI_Manager : MonoBehaviour
 
     void Start()
     {
-        // Subscribe to the video's completion event
-        introVideo.loopPointReached += OnVideoEnded;
-
         // Get the main audio listener in the scene
         audioListener = FindObjectOfType<AudioListener>();
+
+        // Check if this is a level or tutorial video
+        if (SceneManager.GetActiveScene().buildIndex == 1) // Assuming tutorial scene index is 1
+        {
+            // Subscribe to the tutorial video's completion event
+            introVideo.loopPointReached += OnTutorialVideoEnded;
+        }
+        else if (SceneManager.GetActiveScene().buildIndex == 2) // Assuming level scene index is 2
+        {
+            // Subscribe to the level video's completion event
+            introVideo.loopPointReached += OnLevelVideoEnded;
+        }
     }
 
     void OnEnable()
@@ -53,33 +62,14 @@ public class UI_Manager : MonoBehaviour
         }
     }
 
-    public void PlayGame(){
+    public void PlayGame()
+    {
         choicePanel.SetActive(true);
         MainMenuPanel.SetActive(false);
     }
 
     public void ToMainMenu(){
         SceneManager.LoadSceneAsync(0);
-    }
-
-    public void PlayLevel()
-    {
-        choicePanel.SetActive(false);
-        if (audioListener != null)
-            audioListener.enabled = false;
-
-        // Check if introVideo is not null before trying to use it
-        if (introVideo != null)
-        {
-            // If video has ended, reset it back to false and start from the beginning
-            if (videoEnded)
-            {
-                ResetVideo();
-            }
-
-            // Play the video
-            introVideo.Play();
-        }
     }
 
     void ResetVideo()
@@ -98,13 +88,70 @@ public class UI_Manager : MonoBehaviour
         }
     }
 
-    void OnVideoEnded(VideoPlayer vp)
+    public void PlayLevel()
     {
-        introVideo.loopPointReached -= OnVideoEnded;
+    choicePanel.SetActive(false);
+    if (audioListener != null)
+        audioListener.enabled = false;
+
+    // Check if introVideo is not null before trying to use it
+    if (introVideo != null)
+    {
+        // If video has ended, reset it back to false and start from the beginning
+        if (videoEnded)
+        {
+            ResetVideo();
+        }
+
+        // Play the video
+        introVideo.Play();
+    }
+    
+    // Set up the event handler for playing levels
+    introVideo.loopPointReached += OnLevelVideoEnded;
+    }
+
+    public void PlayTutorial()
+    {
+        choicePanel.SetActive(false);
+        if (audioListener != null)
+            audioListener.enabled = false;
+
+        // Check if introVideo is not null before trying to use it
+        if (introVideo != null)
+        {
+        // If video has ended, reset it back to false and start from the beginning
+        if (videoEnded)
+        {
+            ResetVideo();
+        }
+
+        // Play the video
+        introVideo.Play();
+    }
+    
+    // Set up the event handler for playing tutorials
+    introVideo.loopPointReached += OnTutorialVideoEnded;
+    }
+
+    void OnLevelVideoEnded(VideoPlayer vp)
+    {
+        introVideo.loopPointReached -= OnLevelVideoEnded;
         if (audioListener != null)
             audioListener.enabled = true;
-        
+
+        // Load the level scene (assuming it's scene index 2)
         SceneManager.LoadSceneAsync(2);
+    }
+
+    void OnTutorialVideoEnded(VideoPlayer vp)
+    {
+        introVideo.loopPointReached -= OnTutorialVideoEnded;
+        if (audioListener != null)
+            audioListener.enabled = true;
+
+        // Load the tutorial scene (assuming it's scene index 1)
+        SceneManager.LoadSceneAsync(1);
     }
 
     public bool VideoEnded()
@@ -114,10 +161,6 @@ public class UI_Manager : MonoBehaviour
 
     public void RestartLevel(){
         SceneManager.LoadSceneAsync(2);
-    }
-
-    public void PlayTutorial(){
-        SceneManager.LoadSceneAsync(1);
     }
 
     public void ChoiceToMainMenu(){
