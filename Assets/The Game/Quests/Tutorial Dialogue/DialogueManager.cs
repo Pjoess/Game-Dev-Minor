9e6024da790aa4d2cdb4c97695a5264069fd979a
Erick <19063989@student.hhs.nl>
2024-06-01 17:48:43 +0200

    Update Some Build Bugs

--- Assets/The Game/Quests/Tutorial Dialogue/DialogueManager.cs
+++ Assets/The Game/Quests/Tutorial Dialogue/DialogueManager.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 using Cinemachine;
-using Microsoft.Unity.VisualStudio.Editor;
 using TMPro;
 // using UnityEditor.Animations;
 using UnityEngine;
