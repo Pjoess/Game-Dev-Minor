02e6172f4d7860c8457b7b6d56c8d713b8ccfc26
HunterFromYharnam <94044683+TudorGrigoras@users.noreply.github.com>
2024-06-12 14:52:59 +0200

    Skip level button for level 3 and level 2 dialogue image

--- Assets/The Game/Characters/Player/Player_Manager.cs
+++ Assets/The Game/Characters/Player/Player_Manager.cs
@@ -140,6 +140,7 @@ public class Player_Manager : MonoBehaviour, IDamageble
             if (Input.GetKeyDown(KeyCode.Alpha1)) SceneManager.LoadScene(1);
             if (Input.GetKeyDown(KeyCode.Alpha2)) SceneManager.LoadScene(2);
             if (Input.GetKeyDown(KeyCode.Alpha3)) SceneManager.LoadScene(3);
+            if (Input.GetKeyDown(KeyCode.Alpha4)) SceneManager.LoadScene(4);
         }
     }
 
